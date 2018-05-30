09e47c88e31f85dc8b2914d045163d8dd5054241
soundharyas <soundharyasss98@gmail.com>
2018-05-30 21:03:44 +1100

    implement

---
+++ src/com/basic/implement.java
@@ -0,0 +1,26 @@
+
+
+package com.basic;
+
+public class implement {
+	public static void main(String[]args)	
+	{
+
+	int n = 2;
+	switch (n)
+	{
+	case 1:
+	System.out.println( "1" );
+	break;
+	case 2:
+	System.out.println( "2" );
+	break;
+	default:
+	System.out.println( "The number is not 1 or 2" );
+	break;
+	}
+
+	}
+
+
+}
