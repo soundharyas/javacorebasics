4070e5905cac02ac2f5b0beb1299730353eaf01e
soundharyas <soundharyasss98@gmail.com>
2018-05-30 20:15:22 +1100

    Conversion

---
+++ src/com/basic/conversion.java
@@ -0,0 +1,24 @@
+
+package com.basic;
+
+public class conversion {
+	public static void main(String[]args) {
+		int i=0;
+		for(i=0;i<10;i++)
+		{
+			if(i == 3) {
+				continue;
+			}
+		System.out.println("for loop i =" +i);
+		
+		
+			if(i== 5)
+			{
+				break;
+			}
+			
+	}
+}
+}
+
+
